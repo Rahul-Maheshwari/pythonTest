05f6fdae6972971c3ac3c2ccb81164eab4f8771a
Rahul-Maheshwari <rstargit@users.noreply.github.com>
2019-05-07 00:15:15 -0700

    commiting sample gui automation code

---
+++ gui1.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pyautogui, time
+
+
+# In[11]:
+
+
+#time.sleep(5)
+pyautogui.hotkey('win', 'd')
+pyautogui.hotkey('win', 'r')
+pyautogui.typewrite('Automation of windows GUI. Test-1', interval=.2)
+
